b7da6ba145890f791a2d22a1258c5ec7437cfd8a
Preston Tang <prestonleetang0123@gmail.com>
2025-04-22 22:54:28 -0400

    Changed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173', 'https://talknyu.prestontang.dev/'
+    origins 'http://localhost:5173', 'https://talknyu.prestontang.dev'
 
     resource '*',
       headers: :any,
